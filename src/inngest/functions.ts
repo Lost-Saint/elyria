45bc03de02dd9065384c6f86e7c582bc6cca0ec2
lost saint <sunymon@protonmail.com>
2025-06-28 18:06:02 -0400

    chore: ai-agent kit

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer.  You summarize in two words.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await summarizer.run(
+      `Summarize the following text: ${event.data.value}`,
+    );
+
+    return { output };
   },
 );
